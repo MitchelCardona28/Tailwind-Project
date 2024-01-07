a3d98a5d7649ee788ba94adc0522e6c134342fef
MitchelCardona28 <michelito428@gmail.com>
2024-01-06 19:06:09 -0500

    mobile fixings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,6 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'bali': "url('./public/imgs/bali.jpg')",
-        'chicago': "url('./public/imgs/chicago.jpg')",
-        'europe': "url('./public/imgs/europe.jpg')",
-        'iceland': "url('./public/imgs/iceland.jpg')",
-        'LA': "url('./public/imgs/LA.jpg')",
-        'miami': "url('./public/imgs/miami.jpg')",
-        'new_york': "url('./public/imgs/new_york.jpg')",
-        'norway': "url('./public/imgs/norway.jpg')",
-        'sanFrancisco': "url('./public/imgs/sanFrancisco.jpg')",
-        'sanFrancisDesktop': "url('./public/imgs/sanFrancisDesktop.jpg')",
-        'seattle': "url('./public/imgs/seattle.jpg')",
-        'switzerland': "url('./public/imgs/switzerland.jpg')",
-        'sydney': "url('./public/imgs/sydney.jpg')",
-        'yosemite': "url('./public/imgs/yosemite.jpg')",
-      },
       backgroundColor: theme => ({
         ...theme('colors'),
         'principal': '#CC2D4A',
